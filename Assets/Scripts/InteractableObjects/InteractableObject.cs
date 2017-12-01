f1c34398eb84dc94156fe13dc4b1b65f5b2de46b
josiro <197367@edu.rocfriesepoort.nl>
2017-12-01 12:11:20 +0100

    update

--- Assets/Scripts/InteractableObjects/InteractableObject.cs
+++ Assets/Scripts/InteractableObjects/InteractableObject.cs
@@ -5,9 +5,14 @@ public abstract class InteractableObject : MonoBehaviour {
 	[Tooltip("Time needed to activate this component")]
 	public float activationDuration = 4f;
 
+	[Tooltip("How long have we looked at the object")]
+	public float hitDuration;
+
 	// This will be set to "True" when the "IsActivated" is called
-	protected bool active;
+	protected bool active = true;
 
 	// This method will be called from the Gaze Interections script
 	public abstract void IsActivated ();
+
+
 }
--- Assets/Scripts/InteractableObjects/PickableObject.cs
+++ Assets/Scripts/InteractableObjects/PickableObject.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PickableObject : InteractableObject {
 
+	[Tooltip("When we pick up the object, how far away from the camera should we hold it?")]
+	[SerializeField]
+	private float distanceFromCamera = 10f;
+	Rigidbody rb;
+
+	private void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	private void FixedUpdate ()
+	{
+		PickableTick ();
+	}
+
 	// This method is called by the gaze control
 	public override void IsActivated ()
 	{
-		
+		active = true;
 	}
 
-	// Use this for initialization
-	void Start () 
+	void PickableTick ()
 	{
-		
+		switch (active) {
+		case true:
+			transform.LookAt (Camera.main.GetComponentInChildren<Transform>().position);
+			if (Vector3.Distance(transform.position, Camera.main.GetComponentInChildren<Transform>().position ) >= 0.5f)
+			{
+				rb.AddForce (transform.forward * 5);
+			} 
+			else 
+			{
+				rb.velocity = new Vector3 (0,0,0);
+			}
+			break;
+		case false:
+			transform.parent = null;
+			break;
+		}
 	}
 }
