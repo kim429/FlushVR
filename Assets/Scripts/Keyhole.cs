6ee7b3b2273127a0e632eabe1906ced76477f3b6
josiro <197367@edu.rocfriesepoort.nl>
2017-12-01 14:07:06 +0100

    Made a key
    I made a key that opens the door

---
+++ Assets/Scripts/Keyhole.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Keyhole : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.transform.name == "key") 
+		{
+			print ("we opened the door");
+			transform.parent.transform.Rotate(new Vector3 (0,90,0));
+		}
+	}
+}
