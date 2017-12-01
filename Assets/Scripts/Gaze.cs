09f693251bf95d9605841a0b4378d6e1ea7b4411
Kevin van der Bij <kevinvdbij@gmail.com>
2017-12-01 10:16:20 +0100

    Created Gaze Script

---
+++ Assets/Scripts/Gaze.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour {
+    public float gazeRange;
+    public float updateRate;
+
+    private Coroutine gazeUpdate;
+    private RaycastHit gazeHit;
+    private GameObject hitObject;
+    private float hitDuration;
+
+    public void Start()
+    {
+        gazeUpdate = StartCoroutine(GazeUpdate());
+    }
+
+    public IEnumerator GazeUpdate()
+    {
+        while (true)
+        {
+            GazeRaycast(updateRate);
+            yield return new WaitForSeconds(updateRate);
+        }
+    }
+
+    public void GazeRaycast(float elapsedTime)
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out gazeHit))
+        {
+            if (gazeHit.transform.gameObject == hitObject)
+            {
+                hitDuration += elapsedTime;
+            }
+            else
+            {
+                hitObject = gazeHit.transform.gameObject;
+            }
+        }
+    }
+}
