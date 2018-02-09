c8891f547d60d9731d816efe461cb854441dc844
Kevin van der Bij <kevinvdbij@gmail.com>
2018-02-09 11:51:06 +0100

    LFS test

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : InteractableObject
+{
+    [Header("Inventory")]
+    public Text itemOne;
+    public Text itemTwo;
+
+    public bool hasKey;
+    public bool hasPlunger;
+
+    public PickableObject key;
+    public PickableObject plunger;
+
+    public override void IsActivated()
+    {
+        base.IsActivated();
+
+        PickableObject pickObject = Gaze.playerSettings.heldItem;
+        if (!pickObject) return;
+        switch (pickObject.type)
+        {
+            case PickableObjectType.KEY:
+                hasKey = true;
+                pickObject.gameObject.SetActive(false);
+                key = pickObject;
+                Gaze.playerSettings.heldItem = null;
+                break;
+            case PickableObjectType.PLUNGER:
+                hasPlunger = true;
+                pickObject.gameObject.SetActive(false);
+                plunger = pickObject;
+                Gaze.playerSettings.heldItem = null;
+                break;
+        }
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (hasKey == false)
+        {
+            itemOne.color = Color.grey;
+        }
+        if (hasPlunger == false)
+        {
+            itemTwo.color = Color.grey;
+        }
+        if (hasKey == true)
+        {
+            itemOne.color = Color.black;
+        }
+        if (hasPlunger == true)
+        {
+            itemTwo.color = Color.black;
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/MenuItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItem : InteractableObject
+{
+    public PickableObjectType type;
+    public Inventory inventory;
+
+    public override void IsActivated()
+    {
+        base.IsActivated();
+
+        switch (type)
+        {
+            case PickableObjectType.KEY:
+                if (inventory.hasKey)
+                {
+                    inventory.key.gameObject.SetActive(true);
+                    inventory.hasKey = false;
+                    if (Gaze.playerSettings.heldItem)
+                    {
+                        Gaze.playerSettings.heldItem.Deactivate();
+                    }
+                    Gaze.playerSettings.heldItem = inventory.key;
+                }
+            break;
+            case PickableObjectType.PLUNGER:
+                if (inventory.hasPlunger)
+                {
+                    inventory.plunger.gameObject.SetActive(true);
+                    inventory.hasPlunger = false;
+                    if (Gaze.playerSettings.heldItem)
+                    {
+                        Gaze.playerSettings.heldItem.Deactivate();
+                    }
+                    Gaze.playerSettings.heldItem = inventory.plunger;
+                }
+                break;
+        }
+    }
+
+}
